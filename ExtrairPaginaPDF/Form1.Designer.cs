daf3eaabfeccbcc9327c49d98a079532eac7b208
Mateus <mateusitau.unibanco@gmail.com>
2023-02-09 14:44:43 -0300

    Adicionar arquivos de projeto.

---
+++ ExtrairPaginaPDF/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace ExtrairPaginaPDF
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCarregarPDF = new System.Windows.Forms.Button();
+            this.labelNomeArquivo = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnCarregarPDF
+            // 
+            this.btnCarregarPDF.Location = new System.Drawing.Point(273, 140);
+            this.btnCarregarPDF.Name = "btnCarregarPDF";
+            this.btnCarregarPDF.Size = new System.Drawing.Size(94, 36);
+            this.btnCarregarPDF.TabIndex = 0;
+            this.btnCarregarPDF.Text = "Carregar PDF";
+            this.btnCarregarPDF.UseVisualStyleBackColor = true;
+            this.btnCarregarPDF.Click += new System.EventHandler(this.btnCarregarPDF_Click);
+            // 
+            // labelNomeArquivo
+            // 
+            this.labelNomeArquivo.AutoSize = true;
+            this.labelNomeArquivo.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.labelNomeArquivo.Location = new System.Drawing.Point(224, 204);
+            this.labelNomeArquivo.Name = "labelNomeArquivo";
+            this.labelNomeArquivo.Size = new System.Drawing.Size(0, 15);
+            this.labelNomeArquivo.TabIndex = 1;
+            this.labelNomeArquivo.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(664, 312);
+            this.Controls.Add(this.labelNomeArquivo);
+            this.Controls.Add(this.btnCarregarPDF);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Button btnCarregarPDF;
+        private Label labelNomeArquivo;
+    }
+}
---
+++ ExtrairPaginaPDF/Form1.cs
@@ -0,0 +1,112 @@
+using iText.Kernel.Pdf;
+using iText.Kernel.Pdf.Canvas.Parser.Listener;
+using iText.Kernel.Pdf.Canvas.Parser;
+
+namespace ExtrairPaginaPDF
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void btnCarregarPDF_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+
+            openFileDialog.Filter = "Carregar PDF | *.pdf";
+
+            openFileDialog.Multiselect = false;
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string path = openFileDialog.FileName;
+
+                string[] pathSplit = path.Split("-");
+
+                labelNomeArquivo.Text = "Nome do arquivo PDF: " + pathSplit[2];
+            }
+            else return;
+
+            string dirPdf = @"C:\Users\Mateus-dev\source\repos\ExtrairPaginaPDF\ExtrairPaginaPDF\dir_PDF";
+
+            ExtrairPaginaPDF(openFileDialog.FileName, dirPdf);
+        }
+
+
+        private void ExtrairPaginaPDF(string path, string dirPDF)
+        {
+            var pagina = 0;
+            string cpf = "";
+            try
+            {
+                using (var pdf = new PdfReader(path))
+                {
+                    using (var doc = new PdfDocument(pdf))
+                    {
+                        if (doc.GetNumberOfPages() == 0)
+                            MessageBox.Show("O arquivo " + path + " não tem nenhuma página!");
+                        else
+                        {
+                            for (pagina = 1; pagina <= doc.GetNumberOfPages(); pagina++)
+                            {
+                                ITextExtractionStrategy strategy = new SimpleTextExtractionStrategy();
+
+                                string conteudo = PdfTextExtractor.GetTextFromPage(doc.GetPage(pagina), strategy);
+
+                                string novoConteudo = conteudo.Remove(0, 528);
+
+                                string novoConteudoRemovido = novoConteudo.Replace("\nNumero Cartão Nome C.P.F.\nGrau de\nParentesco\nNascimento Valor\n", "");
+
+                                string[] novoConteudoRemovidoSplit = novoConteudoRemovido.Split("\n");
+
+                                string posicao = novoConteudoRemovidoSplit[0]
+                                    .Replace("Titular", "")
+                                    .Replace("/", "")
+                                    .Replace("-", "")
+                                    .Replace("R$", "")
+                                    .Replace(",", "")
+                                    .Replace(".", "");
+
+                                char[] arrChar = posicao.ToCharArray();
+
+                                Array.Reverse(arrChar);
+
+                                string stringInvertida = new String(arrChar);
+
+                                string stringInvertidaRemove = stringInvertida.Remove(0, 17);
+
+                                string stringInvertidaRemove2 = stringInvertidaRemove.Remove(12);
+
+                                char[] arrChar2 = stringInvertidaRemove2.ToCharArray();
+
+                                Array.Reverse(arrChar2);
+
+                                string stringOriginal = new String(arrChar2);
+
+                                cpf = stringOriginal;
+
+                                using (var pdfNovo = new PdfWriter(dirPDF + "\\pag-" + pagina + "-" + cpf + ".pdf"))
+                                {
+                                    using (var docNovo = new PdfDocument(pdfNovo))
+                                    {
+                                        doc.CopyPagesTo(pagina, pagina, docNovo);
+                                    }
+                                }
+
+                            }
+                        }
+                        pagina -= 1;
+                        MessageBox.Show(pagina + " páginas extraídas com sucesso!");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro: " + ex.Message);
+            }
+        }
+    }
+}
